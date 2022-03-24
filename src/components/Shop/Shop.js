4126f31670317c8a339ec03c4b880d9b6c9cd965
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-24 20:34:33 +0600

    first commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,11 +15,38 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
-    const handleAddToCart = (product) =>{
-        console.log(product);
+    useEffect( () =>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product=> product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }  
+        }
+        setCart(savedCart);
+        // console.log('local storage finished')
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart= [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+             const rest = cart.filter(product => product.id !== selectedProduct.id); 
+             exists.quantity = exists.quantity + 1;
+             newCart = [...rest,exists];
+        }
         // do not do this: cart.push(product);
-        const newCart = [...cart, product];
+        
         setCart(newCart);
+        addToDb(selectedProduct.id);
     }
 
     return (
