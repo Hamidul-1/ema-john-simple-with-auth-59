aae4e23bdde393facd22765a0dabb01abb674ffe
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-31 13:41:19 +0600

    added function

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+
+    const handleRemoveProduct = product =>{
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+    }
+
     return (
-        <div>
-            <h2>This is orders: {products.length}</h2>
+        <div className='shop-container'>
+            <div className="review-items-container">
+                {
+                    cart.map(product =><ReviewItem
+                        key={product.id}
+                        product = {product}
+                        handleRemoveProduct={handleRemoveProduct}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">                <Cart cart={cart}></Cart>
+            </div>
+            
         </div>
     );
 };
