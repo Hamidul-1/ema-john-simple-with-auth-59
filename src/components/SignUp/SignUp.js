264445d0bb7c11ae137f76d8289e816daf546f01
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-12 00:44:38 +0600

    sign in setup

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value);
+    }
+
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        if(password !== confirmPassword){
+            setError('Your two passwords did not match');
+            return;
+        }
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Sign Up</h2>
-                <form>
+                <form onSubmit={handleCreateUser}>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
                     </div>
-                    <input className='form-submit' type="submit" value="Sign Up" />
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required/>
+                    </div>
+                    <p style={{color: 'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" required/>
                 </form>
                 <p>
-                    New to Ema-John? <Link className='form-link' to="/signup">Create an account</Link>
+                    Already have an account? <Link className='form-link' to="/login">Login</Link>
                 </p>
 
             </div>
