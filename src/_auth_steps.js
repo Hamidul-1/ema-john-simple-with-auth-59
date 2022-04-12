4535b57c4ead116dd138acad7d2904ed4a18d959
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-12 16:09:21 +0600

    implemented login and redirect to shop

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -1,11 +1,18 @@
 /**
- * 1.create a new project in console.google.com
+ * 1.create a new firebase project in console.google.com
  * 2.npm install firebase
  * 3.create firebase.init.js and import getAuth to export auth
  * 4.Firebase setting > Authentication > enable Email and password auth
  * 5.Create Login, Signup component, setup route
  * 6.attach form field handler and form submit handler
+ * 7.npm install --save react-firebase-hooks
+ * 8.useSignInWithEmailAndPassword from react-firebase-hooks
+ * 9.if user is created redirect to the expected page
+ * 10.useSignInWithEmailAndPassword for sign In
  */
+
+
+
 
 
 /***
